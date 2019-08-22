f7360000bec5c633c8e1c951f2e4e5f71b4fdcf1
Victor <kgs0142@gmail.com>
2019-08-22 14:19:13 +0800

    Basically only change the social about bar. WIP

---
+++ assets/js/custom/mailchimp.js
@@ -0,0 +1,25 @@
+(function() 
+{
+	var triggerBttn = document.getElementById( 'trigger-mailchimp-overlay' ),
+		overlay = document.querySelector( 'div.overlay' ),
+		closeBttn = overlay.querySelector( 'button.overlay-close' );
+		transEndEventNames = {
+			'WebkitTransition': 'webkitTransitionEnd',
+			'MozTransition': 'transitionend',
+			'OTransition': 'oTransitionEnd',
+			'msTransition': 'MSTransitionEnd',
+			'transition': 'transitionend'
+		},
+		transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
+		support = { transitions : Modernizr.csstransitions };
+
+	function toggleOverlay() 
+	{
+		require([“mojo/signup-forms/Loader”], function(L) { L.start({"baseUrl":"mc.us3.list-manage.com","uuid":"32db4b399ec111f22d6aa8566","lid":"6cd4fb8768","uniqueMethods":true}) });
+
+		document.cookie = “MCPopupClosed=; expires=Thu, 01 Jan 1970 00:00:00 UTC”;
+	}
+
+	triggerBttn.addEventListener( 'click', toggleOverlay );
+	closeBttn.addEventListener( 'click', toggleOverlay );
+})();
