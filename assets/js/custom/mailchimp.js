7b4c41955cb20d0afe6c3f9ade93b2498d5e0929
Victor <kgs0142@gmail.com>
2019-08-22 14:29:09 +0800

    Experimenting with MailChimp popup.

--- assets/js/custom/mailchimp.js
+++ assets/js/custom/mailchimp.js
@@ -15,9 +15,10 @@
 
 	function toggleOverlay() 
 	{
-		require([“mojo/signup-forms/Loader”], function(L) { L.start({"baseUrl":"mc.us3.list-manage.com","uuid":"32db4b399ec111f22d6aa8566","lid":"6cd4fb8768","uniqueMethods":true}) });
+		require(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us3.list-manage.com","uuid":"32db4b399ec111f22d6aa8566","lid":"6cd4fb8768","uniqueMethods":true}) });
 
-		document.cookie = “MCPopupClosed=; expires=Thu, 01 Jan 1970 00:00:00 UTC”;
+		document.cookie = "MCPopupClosed=; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+		//document.cookie = "MCPopupClosed=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
 	}
 
 	triggerBttn.addEventListener( 'click', toggleOverlay );
